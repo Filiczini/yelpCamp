852639fa03ff12c06a00c629059c673527e7c09b
Filiczini <filiczini@gmail.com>
2020-05-05 13:46:21 +0200

    add styles, refactor ejs templates

--- app.js
+++ app.js
@@ -19,9 +19,25 @@ let campings = [
     image:
       "https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
   },
+  {
+    name: "Crazy Creak",
+    image:
+      "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Swallow Peak",
+    image:
+      "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    name: "Wonder Moby",
+    image:
+      "https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
 ];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
   res.render("index");
@@ -36,7 +52,7 @@ app.post("/campings", function (req, res) {
   let url = req.body.url;
   let addCamping = { name: name, image: url };
   campings.push(addCamping);
-  res.redirect("campings");
+  res.redirect("/campings");
 });
 
 app.get("/campings/add", function (req, res) {
