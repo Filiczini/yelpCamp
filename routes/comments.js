fd32a8a9181441f108c9d02a84fe0b79fc1b0104
Filiczini <filiczini@gmail.com>
2020-05-14 13:13:01 +0200

    add checkCommentOwner function to comments route, chenge show.ejs (only owner of comment can see buttons edit and delete

--- routes/comments.js
+++ routes/comments.js
@@ -38,7 +38,7 @@ router.post("/", isLoggedIn, function (req, res) {
 });
 
 //EDIT - Edit an exist comment by ID
-router.get("/:comment_id/edit", function (req, res) {
+router.get("/:comment_id/edit", checkCommentOwner, function (req, res) {
   Comment.findById(req.params.comment_id, function (err, foundComment) {
     if (err) {
       console.log(err);
@@ -52,7 +52,7 @@ router.get("/:comment_id/edit", function (req, res) {
 });
 
 //UPDATE - UPDATE an exist comment by ID
-router.put("/:comment_id", function (req, res) {
+router.put("/:comment_id", checkCommentOwner, function (req, res) {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (
     err,
     updateComment
@@ -66,7 +66,7 @@ router.put("/:comment_id", function (req, res) {
 });
 
 //DESTROY - DESTROY an exist comment by ID
-router.delete("/:comment_id", function (req, res) {
+router.delete("/:comment_id", checkCommentOwner, function (req, res) {
   Comment.findByIdAndRemove(req.params.comment_id, function (err) {
     if (err) {
       console.log(err);
@@ -75,6 +75,24 @@ router.delete("/:comment_id", function (req, res) {
     }
   });
 });
+
+function checkCommentOwner(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+      if (err) {
+        redirect("back");
+      } else {
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
 
 // Middleware - loging check func()
 function isLoggedIn(req, res, next) {
